bbad779cbc849a45efaf2055261121e759b53387
Yang Qing-rong <admin@wudimei.com>
2016-08-16 15:53:27 +0800

    update view

--- config/view.php
+++ config/view.php
@@ -3,5 +3,6 @@
 return [
 		'path' => __DIR__ . '/../resources/view' ,
 		'compiled'  => __DIR__ . '/../storage/views_c' ,
-		'forceCompile' => false
+		'forceCompile' => false,
+		'skipCommentTags' => true
 ];
