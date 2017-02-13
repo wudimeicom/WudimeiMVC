7464bf28b86453351c12abb9a67af7f2a8f41d54
Yang Qing-rong <admin@wudimei.com>
2017-02-13 11:53:29 +0800

    language

--- app/Frontend/IndexController.php
+++ app/Frontend/IndexController.php
@@ -8,6 +8,8 @@ class IndexController{
 	{
 		//echo "hello,world";
 		$vars = [];
+		//\Session::set("__language","en");
+		//setcookie('__language','zh-cn',time()+3600*24*365);
 		
 		return  View::make("frontend.index",$vars);
 	}
--- config/lang.php
+++ config/lang.php
@@ -1,5 +1,15 @@
 <?php
 return [
 	'locale' =>    'zh-cn' ,  // 'en',
+	'autoDetectLanguage' => true,
+    /**
+     * mapping for browser's accept language ,it affected  while 'autoDetectLanguage' set as true.
+     * by default,the language folders are under directory 'resources/lang'.
+     * 'language folder name' => ['accept language1',...]
+     */
+    'language_mapping' => [
+        'zh-cn' => ['zh-CN','zh'],
+        'en' => ['en-US','en']
+    ],
 	'path' => __DIR__ . "/../resources/lang"
 ];
